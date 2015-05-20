2af98534754ce0c951e60a19c4bf7d71f1cf204d
Naichin <yonaichin@gmail.com>
2015-05-21 02:26:42 +0800

    sign up with more params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include ActionController::ImplicitRender
+   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+
+   protected
+
+   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :rank, :user_type) }
+   end
   respond_to :json
 end
